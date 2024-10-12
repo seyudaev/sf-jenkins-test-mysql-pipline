b815c47ca1cc3cafb3d617f17ee67b103d8636f3
Sergey Yudaev <seyudaev@gmail.com>
2024-10-12 23:05:10 +0300

    fix

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql --version
-mysql --user rfamro --password none --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam < 'sf-11.5-sql-script1.sql'
+mysql --user rfamro --password none --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam << EOF 'sf-11.5-sql-script1.sql' 
+EOF
